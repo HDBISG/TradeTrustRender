2aa20cf7e4e9937a010ffe66530a1289010338d4
ZhangJi <zhangji@hotmail.com>
2020-09-06 16:14:12 +0800

    template

--- src/templates/coc/template.tsx
+++ src/templates/coc/template.tsx
@@ -12,14 +12,50 @@ const containerStyle = css`
   text-align: center;
 `;
 
+const tableGroup = css`
+  width: 100%; 
+  border: 1px solid grey; 
+  table-layout: fixed; 
+  border-collapse: collapse;
+`;
+
+const td = css`
+  border: 1px solid grey; 
+  border-collapse: collapse;
+`;
+
 export const CocTemplate: FunctionComponent<
   TemplateProps<CocTemplateCertificate> & { className?: string }
 > = ({ document, className = "" }) => {
   return (
     <div css={containerStyle} className={className} id="custom-template">
-      <h1>{document.name}</h1>
-      <div>awarded to</div>
-      <h2>{document.recipient.name}</h2>
+<table css={tableGroup} >
+    <tbody>
+    <tr  >
+    <td css={td}>
+    <p><strong>&nbsp;1 Exporter (Name &amp; Address)</strong></p>
+    <p>PANASONIC LOGISTICS ASIA PACIFIC ON<br />BEHALF OF PANASONIC CORPORATION<br />1006, KADOMA, KADOMA CITY, OSAKA,<br />571-8501, JAPAN</p>
+    </td>
+    <td  css={td} rowspan="2">
+    <p><strong>REPUBLIC OF SINGAPORE</strong></p>
+    <p>Trade Trust</p>
+    <p><strong>CERTIFICATE OF ORIGIN</strong></p>
+    <br/>
+    <p><strong>No. SICC - 88-20-002264</strong></p>
+    <br/>
+    <p>NO UNAUTHORISED ADDITION/ALTERATION MAY BE MADE</p>
+    <p>TO THIS CERTIFICATE ONCE IT IS ISSUED</p>
+    </td>
+    </tr>
+    <tr>
+    <td >
+    <p><strong>&nbsp;2 Consignee (Name, Full Address &amp; Country)</strong></p>
+    <p>PANASONIC INDIA PVT.LTD.<br />6TH FLOOR SPIC BUILDING ANNEXE<br />NO.88, MOUNT ROAD, GUINDY CHENNAI<br />600032, INDIA</p>
+    </td>
+    </tr>
+
+    </tbody>
+    </table>
     </div>
   );
 };
--- src/templates/sample.ts
+++ src/templates/sample.ts
@@ -5,12 +5,60 @@ export interface CocTemplateCertificate extends Document {
   recipient: {
     name: string;
   };
+  eco: {
+      coDocno: string;
+      coRefNo: string;
+      coExptrName: string;
+      coExptrAddr1: string;
+      coExptrAddr2: string;
+      coExptrAddr3: string;
+      coExptrAddr4: string;
+      coConsignName: string;
+      coConsignAddr1: string;
+      coConsignAddr2: string;
+      coConsignAddr3: string;
+      coConsignAddr4: string;
+      coDepartureType: string;
+      coDepartureDt: string;
+      coVslFlt: string;
+      coPortOfDisch: string;
+      coFinaldestCtryCd: string;
+      coOriginCtryDesc: string;
+      coAppliedBy: string;
+      coAppliedLocName: string;
+      coAppliedDt: string;
+      coRemark:string;
+  }
 }
 
 export const cocTemplateCertificate: CocTemplateCertificate = {
   name: "OpenAttestation Tutorial Certificate of Completion",
   recipient: {
     name: "John Doe"
+  },
+  eco: {
+      coDocno: "",
+      coRefNo: "SICC - 88-20-002264",
+      coExptrName: "PANASONIC LOGISTICS ASIA PACIFIC ON BEHALF OF PANASONIC CORPORATION",
+      coExptrAddr1: "1006, KADOMA, KADOMA CITY, OSAKA, 571-8501, JAPAN",
+      coExptrAddr2:"",
+      coExptrAddr3: "",
+      coExptrAddr4: "",
+      coConsignName: "PANASONIC INDIA PVT.LTD.",
+      coConsignAddr1: "6TH FLOOR SPIC BUILDING ANNEXE",
+      coConsignAddr2: "NO.88, MOUNT ROAD, GUINDY CHENNAI",
+      coConsignAddr3: "600032, INDIA",
+      coConsignAddr4: "",
+      coDepartureType: "ON OR ABOUT",
+      coDepartureDt: "2020-01-29",
+      coVslFlt: "NORTHERN_VIVACITY/1748-039S",
+      coPortOfDisch: "CHENNAI VIA PORT KLANG",
+      coFinaldestCtryCd: "INDIA",
+      coOriginCtryDesc: "MALAYSIA",
+      coAppliedBy: "SERENE NEO",
+      coAppliedLocName: "TEAM LEADER",
+      coAppliedDt: "2020-01-31",
+      coRemark:"MANUFACTURER’S NAME AND ADDRESS        PANASONIC SYSTEM NETWORKS MALAYSIA SDN BHD        PLO NO.1 KAWASAN PERINDUSTRIAN SENAI, KB NO.104, 81400 SENAI, JOHOR, MALAYSIA        NOTIFY PARTY:        PANASONIC INDIA PVT.LTD. ( CHENNAI        WAREHOUSE), NO.282/2 ,(A2+B2+C2)        NO 5/240, TIRUVALLUR HIGH RODA,        ALAMADI, PONNERI TALUK , CHENNAI -        600052 , INDIA";
   },
   $template: {
     name: "COC",
